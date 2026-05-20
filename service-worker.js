67bf74e0a120f82ef383f21e11e324f342ce5993
lcampaci <139291659+lcampaci@users.noreply.github.com>
2026-05-20 10:25:17 -0300

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "jogo-de-memoria-windows-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
+// Instalação
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ativação
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar requisições
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
